9c1cd953a64233910e63d0d67870a76651ce545f
yinxiaolong <15121849303@163.com>
2019-04-13 17:23:58 +0800

    1042 Shuffling Machine （20 分)

---
+++ src/ShufflingMachine.java
@@ -0,0 +1,36 @@
+import util.Read;
+
+import java.io.IOException;
+
+public class ShufflingMachine {
+    private static String[] cards = {null,
+            "S1", "S2", "S3", "S4", "S5", "S6", "S7", "S8", "S9", "S10", "S11", "S12", "S13",
+            "H1", "H2", "H3", "H4", "H5", "H6", "H7", "H8", "H9", "H10", "H11", "H12", "H13",
+            "C1", "C2", "C3", "C4", "C5", "C6", "C7", "C8", "C9", "C10", "C11", "C12", "C13",
+            "D1", "D2", "D3", "D4", "D5", "D6", "D7", "D8", "D9", "D10", "D11", "D12", "D13",
+            "J1", "J2"};
+
+    public static void main(String[] args) throws IOException {
+        Read.init();
+        int K = Read.nextInt();
+        String[] ans = new String[55];
+        int[] order = new int[55];
+        for (int i = 1; i < order.length; i++) {
+            order[i] = Read.nextInt();
+        }
+
+        for (int i = 0; i < K; i++) {
+            for (int j = 1; j < order.length; j++) {
+                ans[order[j]] = cards[j];
+            }
+            for (int j = 1; j < ans.length; j++) {
+                cards[j] = ans[j];
+            }
+        }
+
+        for (int i = 1; i < ans.length - 1; i++) {
+            System.out.print(ans[i] + " ");
+        }
+        System.out.println(ans[ans.length - 1]);
+    }
+}
