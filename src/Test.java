83f1dc41193b1be28898c22306c22e82ac430186
yinxiaolong <15121849303@163.com>
2019-05-04 16:13:34 +0800

    Test

--- src/Test.java
+++ src/Test.java
@@ -1,31 +1,7 @@
-public class Test{
-    public static String int2String(int time) {
-        int h = time / 3600;
-        String hh = String.format("%02d", h);
-        time = time % 3600;
-        int m = time / 60;
-        String mm = String.format("%02d", m);
-        h = time % 60;
-        int s = h;
-        String ss = String.format("%02d", s);
-        return hh + ":" + mm + ":" + ss;
-    }
+import java.io.IOException;
 
-    private static int string2Int(String time) {
-        String[] HHMMSS = time.split(":");
-        int h = Integer.parseInt(HHMMSS[0]) * 3600;
-        int m = Integer.parseInt(HHMMSS[1]) * 60;
-        int s = Integer.parseInt(HHMMSS[2]);
-        return h + m + s;
-    }
-    public static void main(String[] args) {
-        StringBuilder ans = new StringBuilder("000123");
-        while (ans.toString().charAt(0) == '0') {
-            ans = ans.replace(0,1,"");
-            if (ans.toString().charAt(0) != '0') {
-                break;
-            }
-        }
-        System.out.println(ans);
+public class Test{
+    public static void main(String[] args) throws IOException {
+        System.out.println( 0% 5);
     }
 }
