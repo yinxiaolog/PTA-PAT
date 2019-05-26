258db3c3616eab58cd0b96ddb42af206138000b1
yinxiaolong <15121849303@163.com>
2019-05-27 00:37:25 +0800

    init commit

---
+++ PAT/cmake-build-debug/CMakeFiles/PAT.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/PAT.dir/main.cpp.o"
+  "PAT.pdb"
+  "PAT"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/PAT.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
